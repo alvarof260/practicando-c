f266571dc781ba83eec9842237a2b280bf050bca
Alvaro Estanislao Figueroa <alvarof260@gmail.com>
2024-04-06 10:12:02 -0300

    feat: ejercicio 1 antes de modificar el programa, numeros aleatorios con arreglos

---
+++ tp6/exercise-1.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define MAX 15
+
+// arr[] es la dirección del puntero
+void fillArray(int arr[], int n, int min, int max);
+void printArray(int arr[], int n);
+float averageOfOddIndex(int arr[], int n);
+void changeNumber(int arr[], int n, int max);
+void incrementOfOdd(int arr[], int n, int step);
+
+
+int main()
+{
+    srand(time(NULL));
+    int arr[MAX];
+    fillArray(arr, MAX, 10, 350);
+    incrementOfOdd(arr, MAX, 1);
+    changeNumber(arr, MAX, 300);
+    printArray(arr, MAX);
+    printf("el promedio es: %.2f", averageOfOddIndex(arr, MAX));
+}
+
+
+void fillArray(int arr[], int n, int min, int max)
+{
+    for (int i = 0; i < n; i++)
+    {
+        arr[i] = rand() % max + min;
+    }
+}
+
+void printArray(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d\n", arr[i]);
+    }
+}
+
+float averageOfOddIndex(int arr[], int n)
+{
+    int sum = 0, count = 0;
+    float average = 0;
+    for(int i = 0; i < n; i++)
+    {
+        if(i % 2 != 0){
+            sum += arr[i];
+            count++;
+        }
+    }
+    average = (float) sum / count;
+    return average;
+}
+
+void changeNumber(int arr[], int n, int max)
+{
+    for (int i = 0; i < n; i++)
+    {
+        if(arr[i] > max)
+        {
+            printf("change number of arr[%d]= %d\n", i, arr[i]);
+            scanf("%d", &arr[i]);
+        }
+    }
+}
+
+void incrementOfOdd(int arr[], int n, int step)
+{
+    for (int i = 0; i < n; i++)
+    {
+        if(arr[i] % 2 != 0)
+            arr[i] = arr[i] + step;
+    }
+}
