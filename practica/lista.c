aa09101d8e25d970ff0a232c30cd1eb1bd1bf101
Alvaro Estanislao Figueroa <alvarof260@gmail.com>
2024-05-01 20:30:44 -0300

    feat: funcion es lista vacia, insertar nodo, mostrar lista

--- practica/lista.c
+++ practica/lista.c
@@ -8,31 +8,53 @@ typedef struct Tnodo
     struct Tnodo *Siguiente;
 } Tnodo;
 
-void crearListaVacia(Tnodo *start);
+void crearListaVacia(Tnodo **start);
+int esListaVacia(Tnodo *start);
+void insertarNodo(Tnodo **start, Item dato);
+void mostrarLista(Tnodo *start);
 
 int main()
 {
     Tnodo *start;
-    crearListaVacia(start);
-    Tnodo *nuevoNodo = malloc(sizeof(Tnodo));
-    nuevoNodo->dato = 25;
-    nuevoNodo->Siguiente = start;
-    start = nuevoNodo;
-    Tnodo *nuevoNodo2 = malloc(sizeof(Tnodo));
-    nuevoNodo2->dato = 40;
-    nuevoNodo2->Siguiente = start;
-    start = nuevoNodo2;
-    Tnodo *aux = start;
-    while (aux != NULL)
-    {
-        printf("el dato del nodo: %d\n", aux->dato);
-        aux = aux->Siguiente;
-    }
+    crearListaVacia(&start);
+    esListaVacia(start) ? printf("Es lista vacia\n") : printf("No es lista vacia\n");
+    insertarNodo(&start, 25);
+    insertarNodo(&start, 40);
+    insertarNodo(&start, 25);
+    insertarNodo(&start, 50);
+    insertarNodo(&start, 80);
+
+    mostrarLista(start);
 
     return 0;
 }
 
-void crearListaVacia(Tnodo *start)
+void crearListaVacia(Tnodo **start)
 {
-    start = NULL;
+    *start = NULL;
+}
+
+int esListaVacia(Tnodo *start)
+{
+    return start == NULL;
+}
+
+void insertarNodo(Tnodo **start, Item dato)
+{
+    Tnodo *nuevoNodo = malloc(sizeof(Tnodo));
+    nuevoNodo->dato = dato;
+    nuevoNodo->Siguiente = *start;
+    *start = nuevoNodo;
+}
+
+void mostrarLista(Tnodo *start)
+{
+    Tnodo *aux = start;
+    int num = 1;
+    while (aux != NULL)
+    {
+        printf("Nodo %d: %d\n", num, aux->dato);
+        num++;
+        aux = aux->Siguiente;
+    }
 }
